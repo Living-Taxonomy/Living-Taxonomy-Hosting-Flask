de3819ebb29ead9a1d8dfaebc23d69f9bc2a28d2
sunbird-sameer <sunbird.sameer@gmail.com>
2022-07-07 15:09:28 +0530

    Update LivingTaxonomy_DB_find.py

--- LivingTaxonomy_DB_find.py
+++ LivingTaxonomy_DB_find.py
@@ -67,7 +67,7 @@ def main(query):
             "<br>"
             "<br>"
 
-            "<iframe src=\"" + video_url + "\"allowfullscreen=\"true\" style=\"width: 100%; height: auto\"></iframe>"
+            "<iframe src=\"" + video_url + "\"allowfullscreen=\"true\" style=\"width: auto; height: auto\"></iframe>"
             "<p>Video by: " + video_credit + "</p>"
 
             "<br>"
